6b25ad836e56c1dbf01faf5cce1b6fee65ff286e
Christina Elena Bornberg <christina.bornberg@outlook.com>
2021-04-04 19:03:34 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,70 +1,52 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
 import matplotlib.pyplot as plt
 
-import cv2
-
-# plot histogram with matplotlib + numpy, recommended
-def np_show_histograms(image):
+def save_hist_and_resul(image, mode):
     
     channels = np.array(image).T
     
     colours = ('r','g','b')
     plt.figure()
     for i, colour in enumerate(colours):      
-        
+        # get histogram for each channel
         histo = np.histogram(channels[i].flatten(), bins=256, range=[0, 256])
-            
-        y_pos = np.arange(len(histo[0]))
-        performance = histo[0]
-        
-        plt.bar(y_pos, performance, align='center', alpha=0.5, color=colour)
+        x_axis = np.arange(len(histo[0]))
+        y_axis = histo[0]
+        # bar plot
+        plt.bar(x_axis, y_axis, align='center', alpha=0.5, color=colour)
 
-# plot histogram with matplotlib + opencv, not recommended
-def cv_show_histograms(image):
+    # save histogram and image results
+    plt.savefig(f"C://Users/Prinzessin/Documents/LifeSci/disseration_images/normalized/python/bhist_{mode}_cell138.png", bbox_inches = 'tight')
+    image.save(f"C://Users/Prinzessin/Documents/LifeSci/disseration_images/normalized/python/bimg_{mode}_cell138.jpg")
     
-    # convert pillow to opencv image
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     
-    colours = ('b','g','r')
-    plt.figure()
-    for i, colour in enumerate(colours):
-        hist = cv2.calcHist([image], [i], None, [256], [0,256])
-        plt.plot(hist, color = colour)
-        plt.xlim([0,256])
-    plt.show()
-
-# plot histogram with matplotlib + numpy, not recommended
-def np_show_histograms_old(image):
-    # get 2D arrays of RGB channels
-    channels = np.array(image).T
+def normalisation_and_equalisation():
     
-    colours = ('r','g','b')
-    plt.figure()
-    for i, colour in enumerate(colours):        
-        # make channel 1D by flatten() and get histogram
-        hist = np.histogram(channels[i].flatten(),   bins=256, range=[0, 256])
-        plt.plot(hist[1][:256], hist[0], color=colour)
-        plt.xlim([0,256])
-    plt.show()
-
-# normalise image with numpy
-def numpy_normalisation():
+    image_original = Image.open("C://Users/Prinzessin/Documents/LifeSci/named_images/cell138.jpg")
     
-    image_original = Image.open("C://Users/Prinzessin/Documents/LifeSci/named_images/cell1.jpg")
-    
+    # (v1) minmax
     image_min = np.min(image_original)
     image_max = np.max(image_original)
     
     min_scale = 0.0
     max_scale = 1.0
         
-    image_scaled = (image_original - image_min) / (image_max - image_min) * (max_scale - min_scale) + min_scale
-    
+    image_scaled_minmax = (image_original - image_min) / (image_max - image_min) * (max_scale - min_scale) + min_scale
     # to pillow
-    image_scaled = (image_scaled*255.0).astype(np.uint8)
-    image_scaled = Image.fromarray(image_scaled, 'RGB')
-    image_scaled.show()
+    image_scaled_minmax = (image_scaled_minmax*255.0).astype(np.uint8)
+    image_scaled_minmax = Image.fromarray(image_scaled_minmax, 'RGB')
+    
+    # (v2) pillow autocontrast
+    image_scaled_autocont =  ImageOps.autocontrast(image_original, cutoff=0, ignore=None)
+    
+    # (v3) pillow equalize
+    image_scaled_equal =  ImageOps.equalize(image_original)
+    
+    # save histograms and image results
+    save_hist_and_resul(image_original, "original")
+    save_hist_and_resul(image_scaled_minmax, "scaled_minmax")
+    save_hist_and_resul(image_scaled_autocont, "scaled_autocon")
+    save_hist_and_resul(image_scaled_equal, "scaled_equal")
 
-    np_show_histograms(image_original)
-    np_show_histograms(image_scaled)
+normalisation_and_equalisation()
